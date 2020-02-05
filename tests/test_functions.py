e90e036b5c2c02ab8a3da4255daba7635205b8de
murphywuwu <murphywuwu@gmail.com>
2020-02-05 23:17:05 +0800

    ✅ (tests/test_functions.py) testGenerators

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,3 +4,38 @@ from __future__ import print_function
 from . import vmtest
 import six
 
+PY3 = six.PY3
+
+class TestFucntion(vmtest.VmTestCase):
+
+
+class TestClosures(vmtest.VmTestCase):
+
+
+class TestGenerators(vmtest.VmTestCase):
+  def test_return_from_generator(self):
+    self.assert_ok("""\
+        def gen():
+          yield 1
+          return 2
+        x = gen()
+        while True:
+          try:
+            print(next(x))
+          except StopIteration as e:
+            print(e.value)
+            break
+      """)
+
+  def test_return_from_generator_with_yield_from(self):
+    self.assert_ok("""\
+        def returner():
+          if False:
+            yield
+          return 1
+
+        def main():
+          y = yeild from returner()
+          print(y)
+        list(main())
+      """)
